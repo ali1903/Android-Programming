96a86f8a15453c3160e3224c9737bd7366e649b8
ali1903 <alihizmetci.1998@gmail.com>
2020-11-20 19:47:48 +0300

    v0.4 Updated
    Spinner and Checkbox added on Textview

--- uygulamadevi1/app/src/main/java/com/example/uygulamadevi1/MainActivity.java
+++ uygulamadevi1/app/src/main/java/com/example/uygulamadevi1/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.uygulamadevi1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinnerr;
     String ogrisimleri[]={"Ali Hizmetçi","Arif Gündüz","Enes Karpuz","Ali Güzel"};
     ArrayAdapter arrayAdapter;
+    TextView textview;
+    CheckBox chc1;
+    CheckBox chc2;
+    CheckBox chc3;
+    CheckBox chc4;
+    CheckBox chc5;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +30,20 @@ public class MainActivity extends AppCompatActivity {
         tanimla();
         adaptorOlustur();
         spinneraEkle();
+        textviewaSpinnerEkle();
     }
     public void tanimla()
     {
         spinnerr = findViewById(R.id.spinner);
+        textview = findViewById(R.id.textview);
+        chc1 = findViewById(R.id.checkc1);
+        chc2 = findViewById(R.id.checkc2);
+        chc3 = findViewById(R.id.checkc3);
+        chc4 = findViewById(R.id.checkc4);
+        chc5 = findViewById(R.id.checkc5);
+
+
+
     }
     public void adaptorOlustur()
     {
@@ -30,4 +53,65 @@ public class MainActivity extends AppCompatActivity {
     {
         spinnerr.setAdapter(arrayAdapter);
     }
+    public void textviewaSpinnerEkle()
+    {
+        spinnerr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textview.setText(spinnerr.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        chc1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chc1.isChecked())
+                {
+                    textview.setText(chc1.getText().toString());
+                }
+            }
+        });
+        chc2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chc2.isChecked())
+                {
+                    textview.setText(chc2.getText().toString());
+                }
+            }
+        });
+        chc3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chc3.isChecked())
+                {
+                    textview.setText(chc3.getText().toString());
+                }
+            }
+        });
+        chc4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chc4.isChecked())
+                {
+                    textview.setText(chc4.getText().toString());
+                }
+            }
+        });
+        chc5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chc5.isChecked())
+                {
+                    textview.setText(chc5.getText().toString());
+                }
+            }
+        });
+    }
+
+
 }
